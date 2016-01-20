795e7a1d1a4ee26ba4ed1ba9f79a92b868485644
Francois polini <Francois@MacBook-Pro-de-Francois.local>
2016-01-20 20:03:30 +0100

    ok

---
+++ includes/fillit.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/13 13:17:02 by fpolini           #+#    #+#             */
+/*   Updated: 2016/01/13 15:39:53 by fpolini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <libft.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# define BUFF_SIZE 546
+
+void	fillit(char *file);
+void	ft_error(void);
+char	*ft_read(char *file);
+void	ft_aff_grid(char *grid);
+int	ft_carre_min(int nb_teti);
+void	ft_full_grid(char *grid, int size);
+void	ft_main(char **teti, int nb_teti);
+void	ft_main_reset(char **grid, char **teti, int i, int size);
+char	*ft_more_grid(char *grid, int size);
+void	ft_reset_grid(char *teti, char *grid, int i, int j);
+int	ft_tall_len(char *str);
+int	ft_teti_put(char *teti, char *grid, int i, int j);
+char	*ft_verif_teti_grid(char *teti, int nb_teti);
+#endif
