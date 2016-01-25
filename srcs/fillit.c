2c05903a5d998aa7ff37bf601bc6da00441d0a48
Francois POLINI <fpolini@e1r10p18.42.fr>
2016-01-25 16:55:27 +0100

    ok

--- srcs/fillit.c
+++ srcs/fillit.c
@@ -6,13 +6,33 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 14:48:32 by fpolini           #+#    #+#             */
-/*   Updated: 2016/01/25 16:23:31 by fpolini          ###   ########.fr       */
+/*   Updated: 2016/01/25 16:55:07 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static char *file1(char **te
+static void	file1(char **tetiss, char *in_file, int j)
+{
+	tetiss[j + 1] = NULL;
+	free(in_file);
+	ft_main(tetiss, j);
+	ft_freetab(tetiss);
+}
+
+static char	*file2(char **tetiss, char *in_file, int j, int i)
+{
+	if (in_file[i] == '\0')
+	{
+		if (ft_strlen(in_file) == 20)
+			tetiss[j] =
+				ft_strdup(ft_teti_verif(ft_strsub(in_file, i - 21, 19), j));
+		else
+			tetiss[j] =
+				ft_strdup(ft_teti_verif(ft_strsub(in_file, i - 20, 19), j));
+	}
+	return (tetiss[j]);
+}
 
 void		fillit(char *file)
 {
@@ -34,18 +54,8 @@ void		fillit(char *file)
 			j++;
 		}
 	}
-	if (in_file[i] == '\0')
-	{
-		if (ft_strlen(in_file) == 20)
-			tetiss[j] = ft_strdup(ft_teti_verif(ft_strsub(in_file, i - 21, 19), j));
-		else
-			tetiss[j] =
-				ft_strdup(ft_teti_verif(ft_strsub(in_file, i - 20, 19), j));
-	}
-	tetiss[j + 1] = NULL;
-	free(in_file);
-	ft_main(tetiss, j);
-	ft_freetab(tetiss);
+	tetiss[j] = file2(tetiss, in_file, j, i);
+	file1(tetiss, in_file, j);
 }
 
 int			main(int argc, char **argv)
